073f0e797cd2a2a90bcd3d2c607b986a5c6884bb
Edgar Ochoa <eochoa3@ucol.mx>
2020-05-15 01:56:36 -0500

    add db for testing

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,22 @@ import path from 'path';
 
 module.exports = {
   development: {
-    database: process.env.DEV_DB_DATABASE || 'codice1',
+    database: process.env.DEV_DB_DATABASE || 'codice_dev',
     username: process.env.DEV_DB_USERNAME || 'codice1',
     password: process.env.DEV_DB_PASSWORD || 'codice1',
     host: process.env.DEV_DB_HOST || 'localhost',
     dialect: 'postgres',
     migrationStorage: 'json',
     migrationStoragePath: path.resolve(__dirname, '..', 'migrations', 'sequelize-meta.json')
+  },
+  test: {
+    database: process.env.DEV_DB_DATABASE || 'codice_test',
+    username: process.env.DEV_DB_USERNAME || 'codice1',
+    password: process.env.DEV_DB_PASSWORD || 'codice1',
+    host: process.env.DEV_DB_HOST || 'localhost',
+    dialect: 'postgres',
+    migrationStorage: 'json',
+    migrationStoragePath: path.resolve(__dirname, '..', 'migrations', 'sequelize-meta-test.json')
   },
   production: {
     database: process.env.PROD_DB_DATABASE,
