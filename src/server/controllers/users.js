f1f36b69a455d057708f51b08581ad7a7eb13c91
Edgar Ochoa <eochoa3@ucol.mx>
2020-10-20 22:57:33 -0500

    improve users controller

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -2,26 +2,32 @@ import express from "express";
 import { User } from "@/models";
 import UniqueConstraintError from "sequelize/lib/errors/validation/unique-constraint-error";
 import ValidationError from "sequelize/lib/errors/validation-error";
+import { paginate } from "@/utils/pagination";
+import { authenticate } from "@/middleware/auth";
 const router = express.Router();
 
-export default router.post("/", async (req, res) => {
-  const { body } = req;
+export default router
+  .post("/", async (req, res) => {
+    const { body } = req;
 
-  try {
-    await User.create(body);
-    return res.status(200).json({ msg: "Usuario creado." });
-  } catch (e) {
-    console.log(e);
-    const type = Object.getPrototypeOf(e);
-    if (type === UniqueConstraintError.prototype) {
-      return res.status(400).json({
-        error: "El nombre de usuario ya existe",
-      });
-    } else if (type === ValidationError.prototype) {
-      return res.status(400).json({
-        error: "La contraseña o el nombre de usuario son inválidos",
-      });
+    try {
+      await User.create(body);
+      return res.status(200).json({ msg: "Usuario creado." });
+    } catch (e) {
+      console.log(e);
+      const type = Object.getPrototypeOf(e);
+      if (type === UniqueConstraintError.prototype) {
+        return res.status(400).json({
+          error: "El nombre de usuario ya existe",
+        });
+      } else if (type === ValidationError.prototype) {
+        return res.status(400).json({
+          error: "La contraseña o el nombre de usuario son inválidos",
+        });
+      }
+      return res.status(500).json({});
     }
-    return res.status(500).json({});
-  }
-});
+  })
+  .get("/", [authenticate, paginate(User)], async (req, res) => {
+    return res.status(200).json(res.paginatedResults);
+  });
