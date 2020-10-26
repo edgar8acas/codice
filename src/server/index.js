773fb698b8d6f80b24c70e99469354ea1cb687e6
Edgar Ochoa <eochoa3@ucol.mx>
2020-10-26 01:21:30 -0600

    check existence of io directory

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,19 @@
-import router from './controllers';
-import initializeServer from './initializeServer';
+import path from "path";
+import fs from "fs";
+import router from "./controllers";
+import initializeServer from "./initializeServer";
+
+/**
+ * Create the directory necessary to communicate with the R process through file reading and writing
+ */
+(function createIoProcessingDirectory() {
+  const dir = path.join(__dirname, "processing", "r", "io");
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+})();
 
 const app = initializeServer(router);
-
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, HOST, () => console.log(`Listening on ${HOST}:${PORT}`));
