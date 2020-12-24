49b03d3281a3e8d585e67c49d4a18b477ffcdf9a
Edgar Ochoa <eochoa3@ucol.mx>
2020-12-23 23:31:40 -0600

    fix cookie error

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -4,11 +4,17 @@ import { checkPassword } from "../utils/helpers";
 import { generateToken, verifyToken } from "../utils/jwt";
 
 const router = express.Router();
-const cookieOptions = {
-  httpOnly: true,
-  maxAge: 1000 * 60 * 60 * 24 * 7,
-  secure: process.env.NODE_ENV === "production",
-};
+
+function getCookieOptions() {
+  const isProd = process.env.NODE_ENV === "production";
+  const options = {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
+  };
+  return options;
+}
 
 export default router
   .post("/", async (req, res) => {
@@ -25,9 +31,12 @@ export default router
         //Issue token
         const token = await generateToken(user);
         const { admin, username, userId } = user;
-        return res.status(200).cookie("authToken", token, cookieOptions).json({
-          user: { admin, username, userId },
-        });
+        return res
+          .status(200)
+          .cookie("authToken", token, getCookieOptions())
+          .json({
+            user: { admin, username, userId },
+          });
       } else {
         throw Error("Password does not match.");
       }
@@ -63,7 +72,7 @@ export default router
     }
   })
   .get("/logout", async (req, res) => {
-    return res.status(200).clearCookie("authToken", cookieOptions).json({
+    return res.status(200).clearCookie("authToken", getCookieOptions()).json({
       msg: "Ha salido exitosamente.",
     });
   });
